019daf149eb6d406b7e1856dafb16e02d115d780
jukim <jukim@osstem.com>
2025-10-24 18:06:20 +0900

    백엔드 날짜 업데이트 안되던 이슈 해결

--- BackEnd/setting.py
+++ BackEnd/setting.py
@@ -1,11 +1,23 @@
 from datetime import datetime
 
-nowYearMonth = str(datetime.today().year) + '.' + str(datetime.today().month) + '.xml'
-XML_KR_SAVEPATH = './Data/Xml_Files/KR/' + nowYearMonth
+# 고정 경로들
 XML_KR_READPATH = 'Data/Xml_Files/KR'
-
-XML_US_SAVEPATH = './Data/Xml_Files/US/' + nowYearMonth
 XML_US_READPATH = 'Data/Xml_Files/US'
+
+# 동적 파일 경로 생성 함수들
+def get_kr_xml_savepath():
+    """현재 날짜 기준으로 한국 XML 파일 경로를 동적으로 생성"""
+    nowYearMonth = str(datetime.today().year) + '.' + str(datetime.today().month) + '.xml'
+    return './Data/Xml_Files/KR/' + nowYearMonth
+
+def get_us_xml_savepath():
+    """현재 날짜 기준으로 미국 XML 파일 경로를 동적으로 생성"""
+    nowYearMonth = str(datetime.today().year) + '.' + str(datetime.today().month) + '.xml'
+    return './Data/Xml_Files/US/' + nowYearMonth
+
+# 하위 호환성을 위한 별칭 (기존 코드가 있을 경우를 대비)
+XML_KR_SAVEPATH = get_kr_xml_savepath()
+XML_US_SAVEPATH = get_us_xml_savepath()
 
 JSON_HISTORY_PATH = './Data/Json_Files/history.txt'
 
